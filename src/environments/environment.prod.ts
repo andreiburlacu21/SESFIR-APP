a15962f4a35ecef5d1327ba4ca25e8921967cbce
Andrei Burlacu <88097674+andreiburlacu21@users.noreply.github.com>
2024-05-21 13:16:02 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: "http://localhost:7226/SESFIR/",
+  isAdmin: false,
+  theme: 'LIGHT',
+  hideSidenav: false,
+  locationX: 0,
+  locationY: 0
 };
