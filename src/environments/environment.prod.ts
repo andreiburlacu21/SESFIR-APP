3fd82e1ed759975caa9d6f81b30f7f0941244383
Andrei Burlacu <88097674+andreiburlacu21@users.noreply.github.com>
2024-05-21 14:47:47 +0300

    API basse url prod update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "http://localhost:7226/SESFIR/",
+  apiBaseUrl: "https://sesfir-b0ec78015824.herokuapp.com/SESFIR/",
   isAdmin: false,
   theme: 'LIGHT',
   hideSidenav: false,
